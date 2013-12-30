17d26229602354a577d0bbee5ca8874271274e2e
tvetan <tvetan@gbg.bg>
2013-12-30 12:05:55 +0200

    Add - Count users with SignalR

---
+++ KeepTrackOfUsersWithSignalR/KeepTrackOfUsersWithSignalR/Hubs/UserActivityHub.cs
@@ -0,0 +1,77 @@
+﻿namespace KeepTrackOfUsersWithSignalR.Hubs
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNet.SignalR;
+    using Microsoft.AspNet.SignalR.Hubs;
+    
+    [HubName("userActivityHub")]
+    public class UserActivityHub : Hub
+    {
+        private static List<string> users = new List<string>();
+
+        public void Send(int count)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<UserActivityHub>();
+            context.Clients.All.updateUsersOnlineCount(count);
+        }
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            string clientId = this.GetClientId();
+            if (users.IndexOf(clientId) == -1)
+            {
+                users.Add(clientId);
+            }
+
+            Send(users.Count);
+
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            string clientId = this.GetClientId();
+            if (users.IndexOf(clientId) == -1)
+            {
+                users.Add(clientId);
+            }
+
+            // Send the current count of users
+            Send(users.Count);
+
+            return base.OnReconnected();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected()
+        {
+            string clientId = this.GetClientId();
+
+            if (users.IndexOf(clientId) > -1)
+            {
+                users.Remove(clientId);
+            }
+
+            // Send the current count of users
+            Send(users.Count);
+
+            return base.OnDisconnected();
+        }
+
+        private string GetClientId()
+        {
+            string clientId = string.Empty;
+           
+            if (Context.QueryString["clientId"] != null)
+            {
+                clientId = this.Context.QueryString["clientId"];
+            }
+
+            if (string.IsNullOrEmpty(clientId.Trim()))
+            {
+                clientId = Context.ConnectionId;
+            }
+
+            return clientId;
+        }
+    }
+}
---
+++ KeepTrackOfUsersWithSignalR/KeepTrackOfUsersWithSignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KeepTrackOfUsersWithSignalR.Startup))]
+namespace KeepTrackOfUsersWithSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
