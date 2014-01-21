f1b5c095c494e6de8eeee28302efa603725c4d0a
tvetan <tvetan@gbg.bg>
2014-01-21 20:23:47 +0200

    Fix Search Home Page style problem.

--- TwitterCopy/TwitterCopy/js/validate_tweet_create_form.js
+++ TwitterCopy/TwitterCopy/js/validate_tweet_create_form.js
@@ -1,22 +1,36 @@
 ﻿$(function () {
-    $("#create-tweet .tweet").attr({ disabled: "disabled" });
 
-    $("#tweetTextArea").on("keyup keypress", function () {
-        $this = $(this);
-        var statusTextLength = 140 - $this.val().length;
-        if (statusTextLength < 140 && statusTextLength >= 0) {
-            $("#create-tweet .tweet").removeAttr("disabled");
-        }
-        else {
-            $("#create-tweet .tweet").attr({ disabled: "disabled" });
-        }
+    function init() {
+        $("#create-tweet .tweet").attr({ disabled: "disabled" });
 
-        $(".tweet-counter").text(statusTextLength);
+        $("#tweetTextArea").on("blur focus", function (event) {
+            $this = $(this)
+            var statusTextLength = $this.val().length;
+            if (statusTextLength === 0) {
+                $(this).toggleClass("tweetTextArea-open");
+            }
+        });
 
-        if (statusTextLength < 0) {
-            $(".tweet-counter").css({ color: "red" })
-        } else {
-            $(".tweet-counter").css({ color: "black" })
-        }
-    });
+        // Calculating the length of the tweet.
+        $("#tweetTextArea").on("keyup keypress", function () {
+            $this = $(this);
+            var statusTextLength = 140 - $this.val().length;
+            if (statusTextLength < 140 && statusTextLength >= 0) {
+                $("#create-tweet .tweet-button").removeAttr("disabled");
+            }
+            else {
+                $("#create-tweet .tweet-button").attr({ disabled: "disabled" });
+            }
+
+            $(".tweet-counter").text(statusTextLength);
+
+            if (statusTextLength < 0) {
+                $(".tweet-counter").css({ color: "red" });
+            } else {
+                $(".tweet-counter").css({ color: "black" });
+            }
+        });
+    }
+
+    init();
 });
